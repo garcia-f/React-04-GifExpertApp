52c1ae754bdeb3954eacf4350b21ac5d4222bada
Fabian <garciafabianemmanuel@gmail.com>
2024-03-21 22:22:35 -0300

    gifgrid

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
   
-    const url = `https://api.giphy.com/v1/gifs/trending?api_key=jJjqPRREhPfWuL0rm66IMkFBZZOOXaO0&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=jJjqPRREhPfWuL0rm66IMkFBZZOOXaO0&q=${ category }&limit=10`;
     const  resp = await fetch( url );
     const { data } = await resp.json();
   
@@ -10,7 +10,6 @@ export const getGifs = async( category ) => {
       url: img.images.downsized_medium.url,
     }))
   
-    console.log( gifs );
     return gifs;
   
   }
